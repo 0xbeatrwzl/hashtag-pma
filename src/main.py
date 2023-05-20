94523c6f8b0a95102d7a707379eb1a469f4f9b3f
Guilherme L <106040818+0xbeatrwzl@users.noreply.github.com>
2023-05-20 20:17:45 -0300

    ...

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return jsonify("a": "a")
+    return 'hello world'
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=80)
