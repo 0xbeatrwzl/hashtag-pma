1b5593829f2cbc6cd05417db6ff42b61808474a9
Guilherme L <106040818+0xbeatrwzl@users.noreply.github.com>
2023-05-20 21:52:19 -0300

    ...

--- src/main.py
+++ src/main.py
@@ -4,11 +4,14 @@ import os
 app = Flask(__name__)
 
 
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    print(request.json())
+
+
 @app.route('/')
 def index():
-    if request.method == "GET":
-        print('a')
-        return render_template('index.html')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
